ed00402f883e96ba9bb52b6efe7b899300840735
samet <sametavcik0@gmail.com>
2022-09-30 22:57:15 +0300

    first commit

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)   // main oluşturulması
+        {
+            double[] girdi1 = { 6, 2, -3, -1, 1, -2, -4, -6 };  // veri setinin girilmesi
+            double[] girdi2= { 5, 4, -5, -1, 1, 7, -2, -3 };  // veri setinin girilmesi
+
+
+
+
+            double[] testVeriA1 = { 1, 2, 2, 1, 2, 1, 3, 3 };  //  test verisi
+            double[] testVeriA2 = { 1, 2, 1, 2, 3, 2, 1, 2};
+
+
+            ArrayList veriler = new ArrayList();   // test  verilerinin tutulduğu arraylist
+
+            // verilerin 10 a bölünmesi
+
+            onaBol(girdi1);     
+            onaBol(girdi2);
+            veriler.Add(onaBol(testVeriA1));
+            veriler.Add(onaBol(testVeriA2));
+
+
+
+            SinirHucresi hücre = new SinirHucresi(girdi1,girdi2);   // sinir hücresinin oluşturulması
+            hücre.agirlikAta(hücre.r, hücre.agirliklar);   // sinir hücresinin ilk agırlıklarının atanması
+
+            int[] targetler = hücre.targetHesapla(hücre.girdi1, hücre.girdi2);  //veri setinin targetlatının atanması
+            int[] ciktilar = new int[girdi1.Length];  // outputların tutulduğu listenin oluşturulması
+            
+            Epok(targetler,ciktilar,hücre,100,veriler);  //epok u yapan fonksiyonun çağırılması
+            
+        }
+        
+        public static void Epok(int[] targetler,int[] ciktilar,SinirHucresi hücre,int epok,ArrayList veriler)  // epok yapan fonksiyon
+        {
+            int totalsayac = 0;
+            for (int k = 0; k < epok; k++)  // kaç epok yapılacaksa o kadar dönen for döngüsü
+            {
+                int sayac1 = 0;
+                for (int i = 0; i < targetler.Length; i++)  // veri setindeki eleman kadar dönen for döngüsü
+                {
+                    int sayac;
+                    ciktilar[i] = hücre.toplamaIslemi(hücre.girdi1[i], hücre.girdi2[i], hücre.agirliklar);  // ilgili verilerin toplam fonksiyonuna sokulup output alınması ve listeye atılması
+                    sayac = hücre.egitim(targetler[i], ciktilar[i], hücre.girdi1[i], hücre.girdi2[i], hücre.agirliklar);  // egitim fonksiyonunun çağırılması
+                    if (sayac == 1)  
+                    {
+                        sayac1 += 1; // doğru değer sayacının bir arttırılması
+                    }
+
+                }
+                totalsayac = sayac1;  // o epoktaki doğru sonuc sayısının total sayaca atılması
+                if(k == 9) { // 10. epokta çalışan if bloğu
+                Console.WriteLine((k + 1) + " " + "Epok sonunda *EĞİTİM* verilerinin  doğruluk oranı = % {0}", (double)sayac1/hücre.girdi1.Length*100);  // 10 epok sonunda eğitim verilerinin doğruluk değerinin yazdırılması
+                for(int i = 0; i < veriler.Count/2; i++)  // test veri seti sayısı kadar çalışan for döngüsü
+                    {
+                        int sayi = hücre.testEgitim((double[])veriler[2*i], (double[])veriler[2*i+1], hücre.agirliklar);  // ilgili test verilerinin hücre sınıfının testeğitim metoduna gönderilmesi
+                        double[] dizi = (double[])veriler[2 * i];  // dizi boyutunun bulunması için değişkene atılması
+                        Console.WriteLine((k + 1) + " " + "Epok sonunda {0}. test veri setinin  doğruluk oranı = % {1}", i+1, (double)sayi / dizi.Length * 100);  // 10 epok sonunda test verilenin doğruluk değerinin yazdırılması
+                    }
+
+                }
+
+        }
+            Console.WriteLine(epok + " " + "Epok sonunda *EĞİTİM* verilerinin doğruluk oranı = % {0}", (double)totalsayac / hücre.girdi1.Length * 100);  // 100 epok sonunda doğruluk değerinin yazdırılması
+            for (int i = 0; i < veriler.Count / 2; i++) // test veri seti sayısı kadar dönen for döngüsü
+            {
+                int x = hücre.testEgitim((double[])veriler[2 * i], (double[])veriler[2 * i + 1], hücre.agirliklar);  // ilgili test verilerinin hücre sınıfının testeğitim metoduna gönderilmesi
+                double[] dizi = (double[])veriler[2 * i]; 
+                Console.WriteLine(epok + " " + "Epok sonunda {0}.test veri setinin doğruluk oranı = % {1}", i+1,(double)x /dizi.Length * 100);  // 100 epok sonunda test verilerinin doğruluk değerinin yazdırılması
+            }
+
+
+
+
+        }
+
+        public static double[] onaBol(double[] dizi1)  // içine gönderilen double tipinde dizinin elemanlarını 10 a bölen ve döndüren metot
+        {
+            for (int i = 0; i < dizi1.Length; i++)
+            {
+                dizi1[i] = dizi1[i] / 10;
+
+            }
+            return dizi1;
+        }
+
+    }
+
+}
---
+++ ConsoleApp13/SinirHucresi.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp13
+{
+    class SinirHucresi
+    {
+        public double[] girdi1;  // veri setindeki x1 leri tutan listenin oluştuurlmaası
+        public double[] girdi2;  // veri setindeki x2 leri tutan listenin oluşturulması
+        public Random r = new Random();  // random değişkenin tutulması
+        public double[] agirliklar = new double[2];  // agirlikları tutan listenin oluşturulması
+
+        public SinirHucresi(double[] girdi1,double[] girdi2)  //  girdi değerlerini parametre olarak alan cons un oluşturulması
+        {
+            this.girdi1 = girdi1;  // girdilerin atanması
+            this.girdi2 = girdi2;
+            
+        }
+
+        public double[] agirlikAta(Random r,double[] agirliklar)  // agırlıkları rastgele atayan fonksiyon
+        {
+            for(int i = 0; i < agirliklar.Length; i++)  // agırlıkların sayısı kadar dönen for döngüsü
+            {
+
+                
+                agirliklar[i] = (r.NextDouble()*2.0)-1.0;  // agırlıkların -1,1 aralığında rastgele atanması
+
+            }
+            return agirliklar;  // listenin geri döndüürlmesi
+        }
+
+        public int toplamaIslemi(double girdi1,double girdi2,double[] agirliklar)  // toplama fonksiyonu
+        {
+            int cikti;   // toplama sonucunda cıktı sonucunun tutulacağı değişkenin atanması
+            double toplam;  // işlem sonucunun tutulduğu değişken
+
+            
+            toplam = 0;
+
+            toplam = (girdi1 * agirliklar[0]) + (girdi2 * agirliklar[1]); // toplama işlemi 
+
+            if(toplam >= 0.5)  // toplamın 0.5 tan büyüklük kontrolü
+            {
+
+             cikti = 1;  // sonucun atanması
+             return cikti; // sonucun döndürülmesi
+
+             }
+             else
+              {
+
+               cikti = -1;
+               return cikti;
+
+              }
+
+            
+            
+        }
+
+        public int[] targetHesapla(double[] girdi1, double[] girdi2)   // targetları hesaplayan fonksiyon
+        {
+            int[] targetler = new int[girdi1.Length];  // target sayısı kadar elemanın olduğu listenin oluşturulması
+            for(int i = 0; i < targetler.Length; i++)  // target sayısı kadar forun dönmesi
+            {
+                if(girdi1[i]+girdi2[i] > 0)  // verilerin toplamı 0 dan büyükse 1 değilse -1 targetinin listeye atılması
+                {
+                    targetler[i] = 1;
+                }
+                else
+                {
+                    targetler[i] = -1;
+                }
+            }
+            return targetler;  // listenin döndürülmesi
+        }
+
+        public int egitim(int target,int cikti,double girdi1,double girdi2, double[] agirliklar)   // egitim fonksiyonu
+        {
+            int sayac = 0;  // egitim sonucunun tutulduğu değişken
+
+            double deger1 = agirliklar[0];  // ilk ağırlığın atanması
+            double deger2 = agirliklar[1];  // ikinci ağırlığın atanması
+
+            if (cikti == -1 && target == 1)  // cıktı -1 se ve target 1 se ağırlıkları arttıran blok
+             {
+
+               deger1 += 0.05 * 2 * girdi1; 
+               deger2 += 0.05 * 2 * girdi2;
+
+             }else if(cikti == 1 && target == -1)  // cıktı 1 ve target -1 se agırlıkları azaltan blok
+              {
+                deger1 += 0.05 * -2 * girdi1;
+                deger2 += 0.05 * -2 * girdi2;
+              }
+              else
+               {
+                 sayac += 1;  // target outputa eşitse sayacın 1 atanması
+               }
+
+            agirliklar[0] = deger1;  // yeni ağırlıkların atanması
+            agirliklar[1] = deger2;
+
+            return sayac; // o veri seti için doğruluk sonucunun geri döndürülmesi
+        }
+        public int testEgitim(double[] testVeri1,double[] testVeri2,double[] agirliklar)  // içine gönderilen double[] tipindeki dizi verilerinin o epok sonundaki ağırlıklarla çarpılarak target değerlerine eşit mi değilmi kontrol eden metot
+        {
+            int sayac = 0;  // test verisindeki doğru verilerin sayısının tutulacağı değişken
+            int[] ciktilar = new int[testVeri1.Length];  // test verilerinin outputlarının tutulacağı dizi
+            int[] targetler = targetHesapla(testVeri1, testVeri2);  // test verilerinin targetlarının hesaplanması
+            for (int a = 0; a < testVeri1.Length; a++)  // veri sayısı kadar dönen for döngüsü
+            {
+
+                ciktilar[a] = toplamaIslemi(testVeri1[a], testVeri2[a], agirliklar);  // çıktıların oluşturulması
+
+                if(ciktilar[a] == targetler[a])  // target == output kontrolü
+                {
+                    sayac += 1;  // sayacın 1 arttırılması
+                }
+
+            }
+
+            return sayac;  // sayacın gerş döndürülmesi
+
+        }
+
+
+    }
+}
